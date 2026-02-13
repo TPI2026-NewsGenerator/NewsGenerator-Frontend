b10183432c801c49dcdebd48cebe9a1bba2182d6
Fabian <rostellofabian@gmail.com>
2026-02-13 11:07:51 +0100

    feat: parse xml using fast-xml-parser

---
+++ server/services/xmlParser.js
@@ -0,0 +1,16 @@
+import { XMLParser} from 'fast-xml-parser';
+
+// XML parsed made with 'fast-xml-parser'
+export const xmlParse = async (xml) => {
+    const parser = new XMLParser();
+    const data = parser.parse(xml);
+
+    return data.rss?.channel?.item?.map(news => ({
+        title: news.title ?? '',
+        thumbnail: news['media:thumbnail'] ?? null,
+        link: news.link ?? null,
+        pubDate: news.pubDate ?? null,
+        description: news.description ?? '',
+        category: news.category ?? null,
+    }));
+}
