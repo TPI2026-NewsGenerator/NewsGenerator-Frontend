e711a9a0e226508522905af0176361d26d35f8ec
Fabian <rostellofabian@gmail.com>
2026-04-28 16:00:02 +0200

    chore: rename file

---
+++ src/features/news-feed/api/news.js
@@ -0,0 +1,22 @@
+//
+//  Author: Fabian Rostello
+//  Date: 03.04.2026
+//  File: news.js
+//  Description: Fetch news api for news feed component
+//
+
+const API_URL = import.meta.env.VITE_API_URL;
+
+export const News = {
+    getNews: async (query) => {
+        const response = await fetch(`${API_URL}/news`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(query),
+        });
+
+        return await response.json();
+    }
+}
