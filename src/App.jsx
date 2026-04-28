3f7bb1c7bae2f83299e2b9050e22969ef7954ace
Fabian <rostellofabian@gmail.com>
2026-04-28 10:40:31 +0200

    chore: rename file

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@ import 'rsuite/dist/rsuite.min.css';
 import 'rsuite/TagPicker/styles/index.css';
 import './App.css'
 import { Routes, Route, Navigate } from "react-router-dom";
-import {FetchPage} from "./pages/FetchPage.jsx";
+import {Fetch} from "./pages/Fetch.jsx";
 
 function App() {
 
     return (
         <Routes>
-            <Route path="/fetch" element={<FetchPage />} />
+            <Route path="/fetch" element={<Fetch />} />
             <Route path="/" element={<Navigate to="/fetch" />} />
         </Routes>
     );
