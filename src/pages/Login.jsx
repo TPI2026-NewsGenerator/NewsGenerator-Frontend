9222642229fd9ae340bfef6281d62d8725282ee4
Fabian <rostellofabian@gmail.com>
2026-04-28 15:51:50 +0200

    feat: add Login page

---
+++ src/pages/Login.jsx
@@ -0,0 +1,93 @@
+//
+//  Author: Fabian Rostello
+//  Date: 03.04.2026
+//  File: Fetch.jsx
+//  Description: Login Page for frontend
+//
+
+import {useRef, useState} from "react";
+import {
+    HStack,
+    VStack,
+    Divider,
+    Box,
+    Heading,
+    Content,
+    Center,
+    Container,
+    CustomProvider,
+    Button,
+    PasswordInput,
+    Form, Schema,
+} from "rsuite";
+import {Login} from '@/features/login/api/login.js'
+
+const {StringType} = Schema.Types;
+const model = Schema.Model({
+    username: StringType().isRequired('This field is required'),
+    password: StringType().isRequired('This field is required'),
+});
+
+
+export const LoginPage = () => {
+    const formRef = useRef();
+    const [formValue, setFormValue] = useState({username: '', password: ''});
+    const [formError, setFormError] = useState({});
+
+    const handleSubmit = async () => {
+        if (!formRef.current.check()) {
+            return;
+        }
+
+        // authenticate user
+        const user = await Login.authUser({
+            username: formValue.username,
+            password: formValue.password
+        })
+    };
+
+    return (
+        <CustomProvider theme="light" alignItems={'center'}>
+            <Container>
+                <Content>
+                    <Center>
+                        <VStack alignItems={'center'} marginTop={50}>
+                            <Heading level={2}>Login</Heading>
+                            <HStack divider={<Divider vertical/>} marginTop={50} h={368} spacing={40} align="flex-start">
+                                <Box>
+                                    <Form layout="vertical"
+                                          ref={formRef}
+                                          model={model}
+                                          formValue={formValue}
+                                          onChange={setFormValue}
+                                          onCheck={setFormError}
+                                          formError={formError}
+                                    >
+                                        <Form.Group controlId='username'>
+                                            <Form.Label>Username</Form.Label>
+                                            <Form.Control
+                                                name="username"
+                                                errorPlacement={'static'}
+                                            />
+                                        </Form.Group>
+                                        <Form.Group controlId={'password'}>
+                                            <Form.Label>Password</Form.Label>
+                                            <Form.Control
+                                                name="password"
+                                                type="password"
+                                                autoComplete="off"
+                                                accepter={PasswordInput}
+                                                errorPlacement={'static'}
+                                            />
+                                        </Form.Group>
+                                        <Button appearance="primary" onClick={handleSubmit}>Login</Button>
+                                    </Form>
+                                </Box>
+                            </HStack>
+                        </VStack>
+                    </Center>
+                </Content>
+            </Container>
+        </CustomProvider>
+    );
+}
