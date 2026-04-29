792f59186aa3a126f5f1c0e0442c43908959557d
Fabian <rostellofabian@gmail.com>
2026-04-29 15:34:58 +0200

    feat: update form and add JWT creation on login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -11,16 +11,19 @@ import {
     VStack,
     Divider,
     Box,
+    Card,
     Heading,
     Content,
     Center,
     Container,
+    Link,
     CustomProvider,
     Button,
     PasswordInput,
     Form, Schema,
 } from "rsuite";
 import {Login} from '@/features/login/api/login.js'
+import { useNavigate } from "react-router-dom";
 
 const {StringType} = Schema.Types;
 const model = Schema.Model({
@@ -33,6 +36,7 @@ export const LoginPage = () => {
     const formRef = useRef();
     const [formValue, setFormValue] = useState({username: '', password: ''});
     const [formError, setFormError] = useState({});
+    const navigate = useNavigate();
 
     const handleSubmit = async () => {
         if (!formRef.current.check()) {
@@ -44,6 +48,11 @@ export const LoginPage = () => {
             username: formValue.username,
             password: formValue.password
         })
+
+        if (user.token) {
+            localStorage.setItem("JWT", user.token);
+            navigate('/fetch')
+        }
     };
 
     return (
@@ -53,37 +62,41 @@ export const LoginPage = () => {
                     <Center>
                         <VStack alignItems={'center'} marginTop={50}>
                             <Heading level={2}>Login</Heading>
-                            <HStack divider={<Divider vertical/>} marginTop={50} h={368} spacing={40} align="flex-start">
-                                <Box>
-                                    <Form layout="vertical"
-                                          ref={formRef}
-                                          model={model}
-                                          formValue={formValue}
-                                          onChange={setFormValue}
-                                          onCheck={setFormError}
-                                          formError={formError}
-                                    >
-                                        <Form.Group controlId='username'>
-                                            <Form.Label>Username</Form.Label>
-                                            <Form.Control
-                                                name="username"
-                                                errorPlacement={'static'}
-                                            />
-                                        </Form.Group>
-                                        <Form.Group controlId={'password'}>
-                                            <Form.Label>Password</Form.Label>
-                                            <Form.Control
-                                                name="password"
-                                                type="password"
-                                                autoComplete="off"
-                                                accepter={PasswordInput}
-                                                errorPlacement={'static'}
-                                            />
-                                        </Form.Group>
-                                        <Button appearance="primary" onClick={handleSubmit}>Login</Button>
-                                    </Form>
-                                </Box>
-                            </HStack>
+                            <Card padding={20} shaded>
+                                <HStack divider={<Divider vertical/>} marginTop={50} spacing={40}
+                                        align="flex-start" alignItems={'center'}>
+                                    <Box>
+                                        <Form layout="vertical"
+                                              ref={formRef}
+                                              model={model}
+                                              formValue={formValue}
+                                              onChange={setFormValue}
+                                              onCheck={setFormError}
+                                              formError={formError}
+                                        >
+                                            <Form.Group controlId='username'>
+                                                <Form.Label>Username</Form.Label>
+                                                <Form.Control
+                                                    name="username"
+                                                    errorPlacement={'static'}
+                                                />
+                                            </Form.Group>
+                                            <Form.Group controlId={'password'}>
+                                                <Form.Label>Password</Form.Label>
+                                                <Form.Control
+                                                    name="password"
+                                                    type="password"
+                                                    autoComplete="off"
+                                                    accepter={PasswordInput}
+                                                    errorPlacement={'static'}
+                                                />
+                                            </Form.Group>
+                                            <Link href="/register">Don't have an account ? Sign up</Link>
+                                            <Button appearance="primary" onClick={handleSubmit} marginTop={50} width={'100%'}>Login</Button>
+                                        </Form>
+                                    </Box>
+                                </HStack>
+                            </Card>
                         </VStack>
                     </Center>
                 </Content>
