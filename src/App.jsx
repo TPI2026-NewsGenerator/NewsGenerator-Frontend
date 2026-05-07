7cabc5e1b9f00cdafc87b9d5d8b2a9e71a3b4edc
Fabian <rostellofabian@gmail.com>
2026-05-07 12:27:27 +0200

    refractor: rename Fetch page to Search page

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import 'rsuite/dist/rsuite.min.css';
 import 'rsuite/TagPicker/styles/index.css';
 import './App.css'
 import { Routes, Route, Navigate } from "react-router-dom";
-import {FetchPage} from "@/pages/Fetch.jsx";
+import {FetchPage} from "@/pages/Search.jsx";
 import {LoginPage} from "@/pages/Login.jsx";
 import {useEffect} from "react";
 import {jwtDecode} from "jwt-decode";
